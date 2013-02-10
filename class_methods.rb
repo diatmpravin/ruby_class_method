441dd1d2d1f1322e07eee925fa26fb1dc95c69e6
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2013-02-10 11:21:44 +0530

    added comment

--- class_methods.rb
+++ class_methods.rb
@@ -1,31 +1,42 @@
+# 1st way
 class Person
-  def self.species
-    p "Homo Sapien"
+  def self.play
+    p "I want to play"
   end
 end
  
+# 2nd way 
 class Person
   class << self
-    def species
-      p "Homo Sapien"
+    def play
+      p "I want to play"
     end
   end
 end
  
+
+
+#Meta programming, add methods on the fly to a class.
+class Person; end
+
+# 3rd way
 class << Person
-  def species
-    p "Homo Sapien"
+  def play
+    p "I want to play"
   end
 end
  
+# 4th way 
 Person.instance_eval do
-  def species
-    p "Homo Sapien"
+  def play
+    p "I want to play"
   end
-end
- 
-def Person.species
-  p "Homo Sapien"
 end
 
-Person.species
+# 5th way
+def Person.play
+  p "I want to play"
+end
+
+
+Person.play
